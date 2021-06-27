980204f94178159a55814146f58ae05ac14a93e1
Dmitry <mazolevichd@gmail.com>
2021-06-28 00:20:57 +0300

    changes after review

--- fibonachi.py
+++ fibonachi.py
@@ -1,13 +1,19 @@
 while True:
-    n = int(input('Write the income number of "Fibonachi": '))
-    if n < 2:
-        print('The "Fibonachi" number is : ' + str(n))
+    try:
+        theinputvalue = int(input('Write the income number of "Fibonachi": '))
+    except ValueError:
+        print('ERROR:the value is not integer')
+        continue
+    if theinputvalue <= 0:
+        print('ERROR: incorrect input')
+    elif theinputvalue < 2:
+        print('The "Fibonachi" number is : ' + str(theinputvalue))
     else :
-        i = 2
-        fib = [0, 1, 1]
-        while i <= n :
-            fib[2] = fib[0] + fib[1]
-            fib[0] = fib[1]
-            fib[1] = fib[2]
-            i += 1
-        print('The "Fibonachi" number is : ' + str(fib[2]))
+        thevariableforloop: int = 2
+        fibonaccisequensarray = [0, 1, 1]
+        while thevariableforloop <= theinputvalue:
+            fibonaccisequensarray[2] = fibonaccisequensarray[0] + fibonaccisequensarray[1]
+            fibonaccisequensarray[0] = fibonaccisequensarray[1]
+            fibonaccisequensarray[1] = fibonaccisequensarray[2]
+            thevariableforloop += 1
+        print('The "Fibonachi" number is : ' + str(fibonaccisequensarray[2]))
